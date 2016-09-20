a3d3a712853170f7e1ee8f1b1e8243e166a73fe0
Johannes Erlandsen <Johannes Erlandsen@DESKTOP-D7HU9DC>
2016-09-20 10:39:25 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07_Timer
+{
+    internal class Timer
+    {
+        private int seconds;
+
+        public int Seconds
+        {
+            get { return seconds; }
+            set { seconds = value; }
+        }
+
+        public override string ToString()
+        {
+            TimeSpan span = TimeSpan.FromSeconds(seconds);
+            return (span.ToString());
+
+
+        }
+
+        internal string Padded(int v)
+        {
+            return v.ToString("00");
+        }
+    }
+}
